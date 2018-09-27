8ba769e515199eb318141c2fa8a19c055311770d
Jorge Salinas <jhsalinas@losheroes.cl>
2018-09-27 16:52:38 -0300

    Se cambia el mensaje

--- src/main/java/io/fabric8/quickstarts/cxf/jaxrs/HelloServiceImpl.java
+++ src/main/java/io/fabric8/quickstarts/cxf/jaxrs/HelloServiceImpl.java
@@ -20,12 +20,12 @@ import io.swagger.annotations.Api;
 @Api("/sayHello")
 public class HelloServiceImpl implements HelloService {
 
-    public String welcome() {
-        return "Welcome to the CXF RS Spring Boot application, append /{name} to call the hello service";
-    }
+	public String welcome() {
+		return "Welcome to the CXF RS Spring Boot application, append /{name} to call the hello service";
+	}
 
-    public String sayHello(String a) {
-        return "Hello " + a + ", Welcome to CXF RS Spring Boot World!!!";
-    }
-    
+	public String sayHello(String a) {
+		return "Hola " + a + ", Bienvenido!!!";
+	}
+
 }
